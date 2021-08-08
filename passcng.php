41bca09f9f8afa03eea094d38f239717e6164122
AfrideHossain <asafridehossain171@gmail.com>
2021-08-08 16:23:52 +0600

    function improved

--- passcng.php
+++ passcng.php
@@ -1,8 +1,25 @@
 <?php
+// include "config/dbcon.php";
+session_start();
+if (!$_COOKIE["loggedin"] == 1) {
+    echo "<script>window.location.href = 'login/login.php';</script>";
+} else {
+    $_SESSION["user_id"] = $_COOKIE["user_id"];
+    $_SESSION["loggedin"] = 1;
+}
+
+?>
+<?php
 $n_pass = file_get_contents("php://input");
 $passobj = json_decode($n_pass);
-//echo $passobj->npass;
+$pass =  $passobj->npass;
+$pass = md5($pass);
+$uid = $_COOKIE["user_id"];
 // $n_pass = $_GET["npass"];
 // echo $n_pass;
-//new line
+include "config/dbcon.php";
+$sql = "UPDATE user_info SET pass = '$pass' WHERE user_id = '$uid'";
+if (mysqli_query($conn, $sql)) {
+    echo "Password has changed";
+}
 ?>
