658d6a9ef6596b4e868a8be76c9640b94d51549e
AfrideHossain <asafridehossain171@gmail.com>
2021-08-28 20:35:23 +0600

    under development

--- confirmorder.php
+++ confirmorder.php
@@ -32,8 +32,16 @@ function input_tester($data)
     return $data;
 }
 $mailer = new PHPMailer(true);
-
+$cart_items = $customer_info->cart; //convert json to object
+$products_str = "<tr> <td> ID </td> <td> Product  </td> <td> Price </td></tr>"; //An empty string for adding products into mbody;
+for ($i = 0; $i > count($cart_items); $i++) {
+    //$products_str .= "<tr> <td>" . $items["product_id"] . "</td> <td>" . $items["product_name"] . "</td> <td>" . $items["price"] . "</td></tr>";
+    echo $cart_items[$i];
+    $products_str .= "<tr> <td>" . $cart_items[$i]["product_id"] . "</td> <td>" . $cart_items[$i]["product_name"] . "</td> <td>" . $cart_items[$i]["price"] . "</td></tr>";
+}
 // Email's body
+/*<span style='background: rgba(0,191,239,0.1); font-size: 1.2em; border: 1px solid #00b1dd; margin: 5px 0px; border-radius: 3px; padding: 10px 30px; margin: 10px 0px; font-weight: bold; text-align: center;'>$products_str
+            </span>*/
 $mbody = "<div style='font-size: 1.2em; padding: 5px;'>
 
             <div style='border-bottom: 1px solid gray;'>
@@ -44,28 +52,35 @@ $mbody = "<div style='font-size: 1.2em; padding: 5px;'>
                 Hello $customer_info->name, <br><br>We have received your request. We will confirm the present status of your order in the return mail as soon as we confirm your order.
             </p>
             <br>
-            <span style='background: rgba(0,191,239,0.1); font-size: 1.2em; border: 1px solid #00b1dd; margin: 5px 0px; border-radius: 3px; padding: 10px 30px; margin: 10px 0px; font-weight: bold; text-align: center;'>$customer_info->cart
-            </span>
+            <table tyle='background: rgba(0,191,239,0.1); font-size: 1.2em; border: 1px solid #00b1dd; margin: 5px 0px; border-radius: 3px; padding: 10px 30px; margin: 10px 0px; font-weight: bold; text-align: center;'>
+            $products_str
+            </table>
             <br>
             <p style='margin-top: 25px;'>Happy Journey</p>
         </div>";
-try {
-    // $mailer->SMTPDebug  = 3;
-    $mailer->isSMTP();
-    $mailer->Host = 'smtp.gmail.com';
-    $mailer->SMTPAuth = true;
-    $mailer->Username = 'maskfreedom093@gmail.com';
-    $mailer->Password = 'asdqwe@#12';
-    $mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mailer->Port = '587';
-    $mailer->setFrom('maskfreedom093@gmail.com');
-    $mailer->addAddress($customer_info->email);
-    $mailer->isHTML(true);
-    $mailer->Subject = "Product confirmation related.";
-    $mailer->Body = $mbody;
-    $mailer->send();
-    echo "Thanks for your contribution.";
-} catch (Exception $e) {
-    echo "An error occured while sending feedback.";
+$clientMailSub = "Product confirmation related.";
+//send mail function
+function sendmail($mailEngine, $toAddr, $mailSub, $mailBody)
+{
+    try {
+        // $mailer->SMTPDebug  = 3;
+        $mailEngine->isSMTP();
+        $mailEngine->Host = 'smtp.gmail.com';
+        $mailEngine->SMTPAuth = true;
+        $mailEngine->Username = 'maskfreedom093@gmail.com';
+        $mailEngine->Password = 'asdqwe@#12';
+        $mailEngine->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mailEngine->Port = '587';
+        $mailEngine->setFrom('maskfreedom093@gmail.com');
+        $mailEngine->addAddress($toAddr);
+        $mailEngine->isHTML(true);
+        $mailEngine->Subject = $mailSub;
+        $mailEngine->Body = $mailBody;
+        $mailEngine->send();
+        echo "Thanks for your contribution.";
+    } catch (Exception $e) {
+        echo "An error occured while sending feedback.";
+    }
 }
+sendmail($mailer, $customer_info->email, $clientMailSub, $mbody);
 ?>
