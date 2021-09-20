742cde12c37cbcde3298c7ffc1c1e274f7991a17
AfrideHossain <asafridehossain171@gmail.com>
2021-09-20 16:42:15 +0600

    second commit

--- confirmorder.php
+++ confirmorder.php
@@ -31,17 +31,36 @@ function input_tester($data)
     $data = htmlspecialchars($data);
     return $data;
 }
-$mailer = new PHPMailer(true);
+//$mailer = new PHPMailer(true);
 $cart_items = $customer_info->cart; //convert json to object
+//print_r($cart_items);
 $products_str = "<tr> <td> ID </td> <td> Product  </td> <td> Price </td></tr>"; //An empty string for adding products into mbody;
 for ($i = 0; $i > count($cart_items); $i++) {
     //$products_str .= "<tr> <td>" . $items["product_id"] . "</td> <td>" . $items["product_name"] . "</td> <td>" . $items["price"] . "</td></tr>";
-    echo $cart_items[$i];
-    $products_str .= "<tr> <td>" . $cart_items[$i]["product_id"] . "</td> <td>" . $cart_items[$i]["product_name"] . "</td> <td>" . $cart_items[$i]["price"] . "</td></tr>";
+    // echo $cart_items[$i];s
+    $item = json_decode($cart_items[$i]);
+    $products_str .= "<tr> <td>" . $item["product_id"] . "</td> <td>" . $item["product_name"] . "</td> <td>" . $item["price"] . "</td></tr>";
 }
 // Email's body
 /*<span style='background: rgba(0,191,239,0.1); font-size: 1.2em; border: 1px solid #00b1dd; margin: 5px 0px; border-radius: 3px; padding: 10px 30px; margin: 10px 0px; font-weight: bold; text-align: center;'>$products_str
             </span>*/
+// $mbody = "<div style='font-size: 1.2em; padding: 5px;'>
+
+//             <div style='border-bottom: 1px solid gray;'>
+//                 <p style='color: red; font-weight: bold; margin: 0; padding: 0; font-size: 2.5em; line-height:0;'>BlueBlood</p>
+//             <p style='color: orange; font-weight: bold; font-size: 1.4em;'>Ornament your heaven</p>
+//             </div>
+//             <p>
+//                 Hello $customer_info->name, <br><br>We have received your request. We will confirm the present status of your order in the return mail as soon as we confirm your order.
+//             </p>
+//             <br>
+//             <table tyle='width: 300px;background: rgba(0,191,239,0.1); font-size: 1.2em; border: 1px solid #00b1dd; margin: 5px 0px; border-radius: 3px; padding: 10px 30px; margin: 10px 0px; font-weight: bold; text-align: center;'>
+//             $products_str
+//             </table>
+//             <br>
+//             <p style='margin-top: 25px;'>Happy Journey</p>
+//         </div>";
+$flink = "http://127.0.0.1/blueblood/images/user_avatar.jpg";
 $mbody = "<div style='font-size: 1.2em; padding: 5px;'>
 
             <div style='border-bottom: 1px solid gray;'>
@@ -52,23 +71,22 @@ $mbody = "<div style='font-size: 1.2em; padding: 5px;'>
                 Hello $customer_info->name, <br><br>We have received your request. We will confirm the present status of your order in the return mail as soon as we confirm your order.
             </p>
             <br>
-            <table tyle='background: rgba(0,191,239,0.1); font-size: 1.2em; border: 1px solid #00b1dd; margin: 5px 0px; border-radius: 3px; padding: 10px 30px; margin: 10px 0px; font-weight: bold; text-align: center;'>
-            $products_str
-            </table>
+            <a href='$flink'>download now</a>
             <br>
             <p style='margin-top: 25px;'>Happy Journey</p>
         </div>";
 $clientMailSub = "Product confirmation related.";
 //send mail function
-function sendmail($mailEngine, $toAddr, $mailSub, $mailBody)
+function sendmail($toAddr, $mailSub, $mailBody)
 {
+    $mailEngine = new PHPMailer(true);
     try {
-        // $mailer->SMTPDebug  = 3;
+        // $mailEngine->SMTPDebug  = 3;
         $mailEngine->isSMTP();
         $mailEngine->Host = 'smtp.gmail.com';
         $mailEngine->SMTPAuth = true;
         $mailEngine->Username = 'maskfreedom093@gmail.com';
-        $mailEngine->Password = 'AsdfQwer@#12'  /*SMTP Pass goes here*/;
+        $mailEngine->Password = ''  /*SMTP Pass goes here*/;
         $mailEngine->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mailEngine->Port = '587';
         $mailEngine->setFrom('maskfreedom093@gmail.com');
@@ -79,8 +97,10 @@ function sendmail($mailEngine, $toAddr, $mailSub, $mailBody)
         $mailEngine->send();
         echo "Thanks for your contribution.";
     } catch (Exception $e) {
-        echo "An error occured while sending feedback.";
+        echo "An error occured while sending feedback.<br>";
+        echo $e->getMessage();
     }
 }
-sendmail($mailer, $customer_info->email, $clientMailSub, $mbody);
+$cmail = $customer_info->email;
+sendmail($cmail, $clientMailSub, $mbody);
 ?>
