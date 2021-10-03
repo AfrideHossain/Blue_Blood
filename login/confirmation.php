f410a77858ff7738cd8ce26542d9845927137c49
AfrideHossain <asafridehossain171@gmail.com>
2021-10-03 21:37:25 +0600

    new added

--- login/confirmation.php
+++ login/confirmation.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="mailver.css" type="text/css" media="all" />
     <title>Confirmation</title>
 </head>
+
 <body>
     <section class="background">
         <form class="form-body" method="post">
@@ -16,13 +18,9 @@
                 </p>
             </div>
             <?php
-            $sqlserver = "localhost";
-            $slquser = 'id16775582_mask007';
-            $password = 'AsdfQwer@#12';
-            $db = 'id16775582_mask';
-            $table = 'valid_users';
-            $conn = mysqli_connect($sqlserver, $slquser, $password, $db);
-            function input_tester($data) {
+            //include("dbcon.php");
+            function input_tester($data)
+            {
 
                 $data = trim($data);
                 $data = stripslashes($data);
@@ -32,17 +30,19 @@
             if (isset($_POST["verify"])) {
                 $pr_code = input_tester($_POST["code"]);
                 if ($pr_code == $_COOKIE["code"]) {
-                    $uname = $_COOKIE["uname"];
-                    $umail = $_COOKIE["mail"];
-                    $sql = "INSERT INTO valid_users(username, user_email) VALUES ('$uname', '$umail')";
-                    try{
-                        mysqli_query($conn, $sql);
-                        mysqli_close($conn);
-                        echo "<div class='msg-bx' style='background-color: rgba(0,255,0, 0.1); border: 1px solid rgb(0,235,10);'><p><a href='index.php' style = 'text-decoration: none; padding: 4px 10px; background: #005fff; color: #fff; border: none; outline: none; border-radius: 3px;'>Let's start Journey</a></p></div>";
-                    }
-                    catch (Exception $e) {
-                        echo $e;
-                    }
+                    // $uname = $_COOKIE["uname"];
+                    // $umail = $_COOKIE["mail"];
+                    setcookie("mailverified", 1, time() + 3600, "/");
+                    echo "<script>window.location.href = 'signup.php';</script>";
+                    //$sql = "INSERT INTO valid_users(username, user_email) VALUES ('$uname', '$umail')";
+                    // try{
+                    //     mysqli_query($conn, $sql);
+                    //     mysqli_close($conn);
+                    //     echo "<div class='msg-bx' style='background-color: rgba(0,255,0, 0.1); border: 1px solid rgb(0,235,10);'><p><a href='index.php' style = 'text-decoration: none; padding: 4px 10px; background: #005fff; color: #fff; border: none; outline: none; border-radius: 3px;'>Let's start Journey</a></p></div>";
+                    // }
+                    // catch (Exception $e) {
+                    //     echo $e;
+                    // }
                     /*mysqli_query($conn, $sql);
                     mysqli_close($conn);
                     //setcookie("mail", "", time() - 3600);
@@ -56,7 +56,6 @@
                 } else {
                     echo "<div class='msg-bx' style='background-color: rgba(255,0,0, 0.1); border: 1px solid rgb(255,0,0); text-align: center;'><p>Confirmation code is wrong.</p></div>";
                 }
-
             }
             ?>
             <input type="text" name="code" id="code" placeholder="Code" required />
@@ -64,4 +63,5 @@
         </form>
     </section>
 </body>
+
 </html>
--- login/handler.php
+++ login/handler.php
@@ -1,11 +1,13 @@
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
 
-require_once('PHPMailer/PHPMailer.php');
-require_once('PHPMailer/Exception.php');
-require_once('PHPMailer/SMTP.php');
+require_once('../PHPMailer/PHPMailer.php');
+require_once('../PHPMailer/Exception.php');
+require_once('../PHPMailer/SMTP.php');
 
-function input_tester($data) {
+function input_tester($data)
+{
 
     $data = trim($data);
     $data = stripslashes($data);
@@ -23,8 +25,8 @@ if (isset($_POST["submit"])) {
     $mbody = "<div style='font-size: 1.2em; padding: 5px;'>
 
             <div style='border-bottom: 1px solid gray;'>
-                <p style='color: red; font-weight: bold; margin: 0; padding: 0; font-size: 2.5em; line-height:0;'>MASK</p>
-            <p style='color: orange; font-weight: bold; font-size: 1.4em;'>Wear a mask and tell the truth</p>
+                <p style='color: red; font-weight: bold; margin: 0; padding: 0; font-size: 2.5em; line-height:0;'>BlueBlood</p>
+            <p style='color: orange; font-weight: bold; font-size: 1.4em;'>Ornament Your Haven</p>
             </div>
             <p>
                 Hello $name, <br><br>Thanks for joining our community. We will keep your informations secret. Here is your confirmation code:
@@ -36,47 +38,40 @@ if (isset($_POST["submit"])) {
             <p style='margin-top: 25px;'>Happy Journey</p>
         </div>";
 
-    $sqlserver = "localhost";
-    $slquser = 'id16775582_mask007';
-    $password = 'AsdfQwer@#12';
-    $db = 'id16775582_mask';
-    $table = 'valid_users';
-    $conn = mysqli_connect($sqlserver, $slquser, $password, $db);
-    $sql = "SELECT username FROM valid_users WHERE user_email = '$mail'";
+    include("dbcon.php");
+    $sql = "SELECT username FROM user_info WHERE email = '$mail'";
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) > 0) {
-        header("Location: index.php");
+        header("Location: login.php");
     } else {
         try {
             /*$mailer->SMTPDebug  = 3;*/
-            $mailer -> isSMTP();
-            $mailer -> Host = 'smtp.gmail.com';
-            $mailer -> SMTPAuth = true;
-            $mailer -> Username = 'maskfreedom093@gmail.com';
-            $mailer -> Password = 'asdqwe@#12';
-            $mailer -> SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            $mailer -> Port = '587';
+            $mailer->isSMTP();
+            $mailer->Host = 'smtp.gmail.com';
+            $mailer->SMTPAuth = true;
+            $mailer->Username = 'maskfreedom093@gmail.com';
+            $mailer->Password = 'QwerAsdf@#12';
+            $mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mailer->Port = '587';
 
-            $mailer -> setFrom('maskfreedom093@gmail.com');
-            $mailer -> addAddress($mail);
-            $mailer -> isHTML(true);
-            $mailer -> Subject = "Confirm to continue at MASK.";
-            $mailer -> Body = $mbody;
-            $mailer -> send();
+            $mailer->setFrom('maskfreedom093@gmail.com');
+            $mailer->addAddress($mail);
+            $mailer->isHTML(true);
+            $mailer->Subject = "Confirm to continue at MASK.";
+            $mailer->Body = $mbody;
+            $mailer->send();
             /*echo "<script> alert('Thanks for contacting us.\nWe will get back to you as soon as possible.'); </script>";*/
             /*setcookie("mail", "", time() - 3600);
         setcookie("uname", "",time() - 3600);
         setcookie("code", "",time() - 3600);*/
-            setcookie("mail", $mail, time() + 300, "/");
-            setcookie("uname", $name, time() + 300, "/");
+            setcookie("mail", $mail, time() + 3600, "/");
+            setcookie("uname", $name, time() + 3600, "/");
             setcookie("code", $security_code, time() + 300, "/");
             header("Location: confirmation.php");
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             echo "An error occured while sending Email.";
         }
     }
 } else {
     header("Location: mailver.php");
 }
-?>
